2d7fb8a9d5bb9719f5445b6433875d834b7606b1
futa125 <ivanfutivic@gmail.com>
2023-04-11 10:57:37 +0200

    Update 6.py

--- lab2/6.py
+++ lab2/6.py
@@ -1,219 +1,252 @@
 from __future__ import annotations
 
 import ast
+import logging
 import re
 import string
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import List, Optional, Set
+from typing import Optional, Set, Dict
 
 
 class Subject(ABC):
     @abstractmethod
-    def attach(self, observer: Observer):
+    def attach(self, observer: Observer) -> None:
         pass
 
     @abstractmethod
-    def detach(self, observer: Observer):
+    def detach(self, observer: Observer) -> None:
         pass
 
     @abstractmethod
-    def detach_all(self):
-        pass
-
-    @abstractmethod
-    def notify(self):
+    def notify(self) -> None:
         pass
 
 
 class Observer(ABC):
     @abstractmethod
-    def update(self):
+    def update(self) -> None:
         pass
 
 
-@dataclass(eq=False)  # Makes the class hashable
-class Cell(Subject, Observer):
-    _sheet: Sheet
-    expression: str
-    value: int = field(init=False)
-    observers: Set[Observer] = field(default_factory=set, init=False)
+@dataclass
+class InvalidAddress(Exception):
+    _value: str
+
+    def __str__(self) -> str:
+        return f"Invalid cell address {self._value}"
+
+
+@dataclass
+class Address:
+    _row: str
+    _column: int
+
+    def __init__(self, value: str) -> None:
+        if not re.match("^([A-Z])([1-9]\\d*)$", value):
+            raise InvalidAddress(value)
+
+        self._row, self._column = re.search("^([A-Z])([1-9]\\d*)$", value).groups()
+
+        if not re.match("^[A-Z]$", self._row):
+            raise InvalidAddress(value)
+
+        if not re.match("^[1-9]\\d*$", str(self._column)):
+            raise InvalidAddress(value)
+
+    def __hash__(self) -> int:
+        return hash((self._row, self._column))
+
+    def __repr__(self) -> str:
+        return f"{self._row}{self._column}"
+
+
+@dataclass
+class Expression:
+    value: str
+    referenced_addresses: Set[Address] = field(default_factory=set, init=False)
 
     def __post_init__(self):
-        self.evaluate()
+        for reference in re.findall("[A-Z][1-9]\\d*", self.value):
+            self.referenced_addresses.add(Address(reference))
 
-    def set_expression(self, expression: str) -> None:
+    def __repr__(self) -> str:
+        return self.value
+
+
+@dataclass
+class Cell(Subject, Observer):
+    _sheet: Sheet
+    _address: Address
+    _observers: Set[Observer] = field(default_factory=set, init=False)
+
+    value: int = field(init=False)
+    expression: Expression
+
+    def __post_init__(self):
+        self._evaluate()
+
+    def set(self, expression: Expression) -> None:
         self.expression = expression
-        self.evaluate()
-
-    def evaluate(self) -> None:
-        self.value = self._sheet.evaluate(self)
-        self.notify()
+        self._evaluate()
 
     def attach(self, observer: Observer):
-        self.observers.add(observer)
+        self._observers.add(observer)
 
     def detach(self, observer: Observer):
-        self.observers.remove(observer)
-
-    def detach_all(self):
-        self.observers.clear()
+        self._observers.remove(observer)
 
     def notify(self):
-        for observer in self.observers:
+        for observer in self._observers:
             observer.update()
 
     def update(self):
-        self.evaluate()
+        self._evaluate()
+
+    def _evaluate(self) -> None:
+        self.current_value = self._sheet.evaluate(self)
+        self.notify()
+
+    def __hash__(self) -> int:
+        return self._address.__hash__()
 
     def __repr__(self) -> str:
         if self.expression == "":
             return ""
 
-        if self.expression == str(self.value):
-            return str(self.value)
+        if self.expression.value == str(self.current_value):
+            return str(self.current_value)
 
-        return f"{self.expression}={self.value}"
+        return f"{self.expression}={self.current_value}"
 
 
 @dataclass
 class Sheet:
     _row_count: int
     _column_count: int
-    _cells: List[List[Optional[Cell]]] = field(init=False, default_factory=list)
+    _cells: Dict[Address, Optional[Cell]] = field(init=False, default_factory=dict)
 
-    def __post_init__(self):
-        for _ in range(self._row_count):
-            self._cells.append([None for _ in range(self._column_count)])
+    def __post_init__(self) -> None:
+        for row_index in range(self._row_count):
+            for column_index in range(self._column_count):
+                letter, number = string.ascii_uppercase[row_index], int(column_index) + 1
+                self._cells[Address(f"{letter}{number}")] = None
 
-        self.print()
+    def cell(self, address: Address) -> Optional[Cell, None]:
+        return self._cells.get(address)
 
-    def cell(self, ref: str):
-        i, j = self._ref_to_index(ref)
+    def set(self, address: Address, expression: Expression) -> None:
+        logging.debug(f"Set value of {address} to {expression}")
 
-        return self._cells[i][j]
+        if self.cell(address) is None:
+            current_referenced_addresses = set()
+            self._cells[address] = Cell(self, address, expression)
+        else:
+            current_referenced_addresses = self.cell(address).expression.referenced_addresses
 
-    def set(self, ref: str, expression: str):
-        if self.cell(ref) is None:
-            i, j = self._ref_to_index(ref)
-            self._cells[i][j] = Cell(self, expression)
+        old_referenced_addresses = current_referenced_addresses.difference(expression.referenced_addresses)
+        new_referenced_addresses = expression.referenced_addresses.difference(current_referenced_addresses)
 
-        self._check_for_cycles(ref, expression)
+        self._check_for_cycles(address, expression)
 
-        new_refs = set(self.get_refs_from_expression(expression))
-        old_refs = set(self.get_refs(self.cell(ref))).difference(new_refs)
+        for old_address in old_referenced_addresses:
+            self.cell(old_address).detach(self.cell(address))
+            logging.debug(f"Cell {address} is no longer observing {old_address}")
 
-        for old_ref in old_refs:
-            self.cell(old_ref).detach(self.cell(ref))
-            print(f"Cell {ref} is no longer observing {old_ref}")
+        for new_address in new_referenced_addresses:
+            self.cell(new_address).attach(self.cell(address))
+            logging.debug(f"Cell {address} is now observing {new_address}")
 
-        for new_ref in new_refs:
-            self.cell(new_ref).attach(self.cell(ref))
-            print(f"Cell {ref} is now observing {new_ref}")
+        self.cell(address).set(expression)
 
-        self.cell(ref).set_expression(expression)
-
-    def _check_for_cycles(self, ref: str, expression: str):
-        cell = self.cell(ref)
-
-        cells_queue = set([self.cell(ref) for ref in self.get_refs_from_expression(expression)])
-        cells_visited = set()
-
-        while cell not in cells_visited and cells_queue:
-            current_cell = cells_queue.pop()
-            if current_cell in cells_visited:
-                continue
-
-            cells_visited.add(current_cell)
-
-            children = [self.cell(ref) for ref in self.get_refs(current_cell)]
-            cells_queue.update([cell for cell in children if cell not in cells_visited])
-
-        if cell in cells_visited:
-            raise RuntimeError("Circular reference detected!")
-
-    def get_refs(self, cell: Cell) -> List[str]:
-        return self.get_refs_from_expression(cell.expression)
-
-    @staticmethod
-    def get_refs_from_expression(expression: str) -> List[str]:
-        return re.findall("[A-Z][0-9]+", expression)
-
-    def evaluate(self, cell: Cell):
-        return self.eval_expression(cell.expression)
-
-    def eval_expression(self, exp) -> int:
+    def evaluate(self, cell: Cell) -> complex | int:
         def _eval(node: ast.expr) -> complex | int:
             if isinstance(node, ast.Num):
                 return node.n
             elif isinstance(node, ast.Name):
-                return self.cell(node.id).value
+                return self.cell(Address(node.id)).current_value
             elif isinstance(node, ast.BinOp):
                 return _eval(node.left) + _eval(node.right)
             else:
                 raise Exception(f"Unsupported type {node}")
 
-        ast_node = ast.parse(exp, mode="eval")
+        ast_node = ast.parse(cell.expression.value, mode="eval")
 
         return _eval(ast_node.body)
 
-    def print(self):
-        for row in self._cells:
-            for item in row:
-                print(f"{str(item):10}", end="\t")
+    def _check_for_cycles(self, address: Address, expression: Expression) -> None:
+        start_cell = self.cell(address)
 
-            print()
+        referenced_cells = set([self.cell(ref) for ref in expression.referenced_addresses])
+        visited_cells = set()
 
-    @staticmethod
-    def _ref_to_index(ref: str) -> (int, int):
-        groups = re.search(r"([A-Z])(\d+)", ref).groups()
-        letter, number = groups
+        while start_cell not in visited_cells and len(referenced_cells) > 0:
+            current_cell = referenced_cells.pop()
+            if current_cell in visited_cells:
+                continue
 
-        return string.ascii_uppercase.index(letter), int(number) - 1
+            visited_cells.add(current_cell)
+            referenced_cells.update([self.cell(address) for address in current_cell.expression.referenced_addresses])
+
+        if start_cell in visited_cells:
+            raise ValueError(f"Circular reference detected")
+
+    def __repr__(self) -> str:
+        output = ""
+        cell_sizes = [len(str(cell)) for cell in self._cells.values() if cell]
+
+        if len(cell_sizes) == 0:
+            longest_cell = len(str(None))
+        else:
+            longest_cell = max(cell_sizes)
+
+        for i, value in enumerate(self._cells.values()):
+            if i != 0 and i % self._column_count == 0:
+                output += "\n"
+
+            output += f"{str(value):{longest_cell + 2}}"
+
+        output += "\n"
+
+        return output
 
 
 def main() -> None:
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
     s = Sheet(5, 5)
-    print()
+    print(s)
 
-    s.set("A1", "2")
-    s.set("A2", "5")
-    s.set("A3", "A1+A2")
-    s.print()
-    print()
+    s.set(Address("A1"), Expression("2"))
+    s.set(Address("A2"), Expression("5"))
+    s.set(Address("A3"), Expression("A1+A2"))
+    print(s)
 
-    s.set("A1", "4")
-    s.set("A4", "A1+A3")
-    s.print()
-    print()
+    s.set(Address("A1"), Expression("4"))
+    s.set(Address("A4"), Expression("A1+A3"))
+    print(s)
 
     try:
-        s.set("A1", "A3")
-    except RuntimeError as e:
+        s.set(Address("A1"), Expression("A3"))
+    except ValueError as e:
         print(f"Caught exception: {e}")
 
-    s.print()
-    print()
+    print(s)
 
-    s.set("A4", "2")
-    s.print()
-    print()
+    s.set(Address("A4"), Expression("2"))
+    print(s)
 
-    s.set("B3", "A1+A3+A4")
-    s.print()
-    print()
+    s.set(Address("B3"), Expression("A1+A3+A4"))
+    print(s)
 
-    s.set("A1", "5")
-    s.print()
-    print()
+    s.set(Address("A1"), Expression("5"))
+    print(s)
 
-    s.set("C5", "B3+A3")
-    s.print()
-    print()
+    s.set(Address("C5"), Expression("B3+A3"))
+    print(s)
 
-    s.set("A1", "6")
-    s.print()
-    print()
+    s.set(Address("A1"), Expression("6"))
+    print(s)
 
 
 if __name__ == "__main__":
